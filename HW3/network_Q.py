14dc9349bba2b967ad16d85c94e02d81079f6e12
Moshey99 <moshe14199958@gmail.com>
2024-07-08 01:27:50 +0300

    remove todos

--- HW3/network_Q.py
+++ HW3/network_Q.py
@@ -10,7 +10,6 @@ env = gym.make('FrozenLake-v0')
 
 # Define the neural network mapping 16x1 one hot vector to a vector of 4 Q values
 # and training loss
-# TODO: define network, loss and optimiser(use learning rate of 0.1).
 
 q_model = nn.Linear(16, 4, bias=False)
 loss_fn = nn.MSELoss(reduction='sum')
@@ -36,7 +35,6 @@ for i in range(num_episodes):
         j += 1
         # 1. Choose an action greedily from the Q-network
         #    (run the network for current state and choose the action with the maxQ)
-        # TODO: Implement Step 1
         s_one_hot = nn.functional.one_hot(torch.tensor(s), num_classes=16).float().reshape(1,-1)
         Q = q_model(s_one_hot)
         a = torch.argmax(Q).item()
@@ -48,15 +46,12 @@ for i in range(num_episodes):
         s1, r, d, _ = env.step(a)
 
         # 4. Obtain the Q'(mark as Q1) values by feeding the new state through our network
-        # TODO: Implement Step 4
         s1_one_hot = torch.nn.functional.one_hot(torch.tensor(s1), num_classes=16).float().reshape(1,-1)
         Q1 = q_model(s1_one_hot)
         # 5. Obtain maxQ' and set our target value for chosen action using the bellman equation.
-        # TODO: Implement Step 5
         Q_target = Q.clone()
         Q_target[:,a] = r + y * torch.max(Q1).item()
         # 6. Train the network using target and predicted Q values (model.zero(), forward, backward, optim.step)
-        # TODO: Implement Step 6
         optimizer.zero_grad()
         loss = loss_fn(Q_target, Q)
         loss.backward()
